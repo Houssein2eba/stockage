da870c928524b3c1fae8a5a0247f6f7ac1890b4a
houssein ebe <hanimation410@gmail.com>
2025-06-12 09:15:21 +0000

    qeueu notification

--- app/Observers/ProductObserver.php
+++ app/Observers/ProductObserver.php
@@ -2,7 +2,8 @@
 
 namespace App\Observers;
 
-use App\Models\Fcm;
+use App\Jobs\SendFcmLowProductNotification;
+        use App\Models\Fcm;
         use App\Models\Product;
 use App\Models\Setting;
 use App\Models\User;
@@ -24,43 +25,27 @@ class ProductObserver
      */
     public function updated(Product $product): void
 {
-    $last_notified_quantity = $product->last_notified_quantity;
+    // $last_notified_quantity = $product->last_notified_quantity;
     $previous_quantity = $product->getOriginal('quantity');
     $current_quantity = $product->quantity;
     $low_threshold = 10;
 
-    if ($current_quantity <= $low_threshold && $previous_quantity != $last_notified_quantity) {
+    if ($current_quantity <= $low_threshold
+    // && $previous_quantity != $last_notified_quantity
+    ) {
         $message = "Le produit {$product->name} est en rupture de stock. Quantité actuelle : {$current_quantity}";
 
         // Optimize by sending to all users at once
         $users = User::select('id')->get(); // Only get IDs
-        Notification::send($users, new \App\Notifications\LowProductNotification($message));
-        //fcm
-        $credentialsPath = config('services.firebase.credentials');
-if (!file_exists($credentialsPath)) {
-    logger("Firebase credentials not found at: " . $credentialsPath);
-    return;
-}else{
-    logger("Firebase credentials found at: ". $credentialsPath);
-}
-        $factory = (new Factory)->withServiceAccount(config('services.firebase.credentials'));
+         Notification::send($users, new \App\Notifications\LowProductNotification($message));
 
+    // Dispatch FCM job
+    SendFcmLowProductNotification::dispatch($message);
 
-        $messaging = $factory->createMessaging();
-
-        $token = Fcm::latest()->value('token');
-                    $messages = [
-                'token' => $token,
-                'notification' => [
-                    'title' => "low product",
-                    'body' => $message,
-                ],
-            ];
-            $messaging->send($messages);
-        // Update the last notified quantity without triggering another update
-        $product->withoutEvents(function () use ($product, $current_quantity) {
-            $product->update(['last_notified_quantity' => $current_quantity]);
-        });
+    // Update the last notified quantity without triggering another update
+    // $product->withoutEvents(function () use ($product, $current_quantity) {
+    //     $product->update(['last_notified_quantity' => $current_quantity]);
+    // });
     }
 }
 
